7e6bd378ab215ba815f26eb790fc737ba777e24f
ojvl1 <antompietriosvaldo@gmail.com>
2025-04-11 11:28:59 -0400

    Password

--- 0x02-dart-classes/2-password.dart
+++ 0x02-dart-classes/2-password.dart
@@ -1,17 +1,17 @@
-class Passsword {
-  String passsword = '';
+class Password {
+  String password = '';
 
   bool isValid() {
-    final hasUppercase = passsword.contains(RegExp(r'[A-Z]'));
-    final hasLowercase = passsword.contains(RegExp(r'[a-z]'));
-    final hasNumber = passsword.contains(RegExp(r'0-9'));
-    final correctLength = passsword.length >= 8 && passsword.length <= 16;
+    final hasUppercase = password.contains(RegExp(r'[A-Z]'));
+    final hasLowercase = password.contains(RegExp(r'[a-z]'));
+    final hasNumber = password.contains(RegExp(r'0-9'));
+    final correctLength = password.length >= 8 && password.length <= 16;
 
     return hasUppercase && hasLowercase && hasNumber && correctLength;
   }
 
   @override
   String toString() {
-    return 'Your Password is: $passsword';
+    return 'Your Password is: $password';
   }
 }
