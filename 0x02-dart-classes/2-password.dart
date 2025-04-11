246250a9c3a040a9ec41a27c39fb68fda6697fb0
ojvl1 <antompietriosvaldo@gmail.com>
2025-04-11 14:50:39 -0400

    Password

--- 0x02-dart-classes/2-password.dart
+++ 0x02-dart-classes/2-password.dart
@@ -4,7 +4,7 @@ class Password {
   bool isValid() {
     final hasUppercase = password.contains(RegExp(r'[A-Z]'));
     final hasLowercase = password.contains(RegExp(r'[a-z]'));
-    final hasNumber = password.contains(RegExp(r'0-9'));
+    final hasNumber = password.contains(RegExp(r'[0-9]'));
     final correctLength = password.length >= 8 && password.length <= 16;
 
     return hasUppercase && hasLowercase && hasNumber && correctLength;
