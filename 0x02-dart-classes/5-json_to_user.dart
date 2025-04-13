91c75e6b152ae576c815b303c2d4147d30bae50f
ojvl1 <antompietriosvaldo@gmail.com>
2025-04-13 17:03:07 -0400

    Json to User fix

--- 0x02-dart-classes/5-json_to_user.dart
+++ 0x02-dart-classes/5-json_to_user.dart
@@ -1,26 +1,26 @@
 class User {
+  int id;
   String name;
   int age;
   double height;
-  int id;
 
-  User({required this.name, required this.age, required this.height, required this.id});
+  User({required this.id, required this.name, required this.age, required this.height});
 
   Map<String, dynamic> toJson() {
     return {
+      'id': id,
       'name': name,
       'age': age,
       'height': height,
-      'id': id
     };
   }
 
   static User fromJson(Map<dynamic, dynamic> userJson) {
     return User(
+      id: userJson['id'],
       name: userJson['name'],
       age: userJson['age'],
       height: userJson['height'].toDouble(),
-      id: userJson['id']
     );
   }
   
